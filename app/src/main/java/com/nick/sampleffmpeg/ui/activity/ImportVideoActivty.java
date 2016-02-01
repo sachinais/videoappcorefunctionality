ae854b102814561fbb390ec0d9d2047ad5d92f9b
Admin <conradmediapty@gmail.com>
2016-02-01 14:13:39 +0530

    Sachin  commit.

--- app/src/main/java/com/nick/sampleffmpeg/ui/activity/ImportVideoActivty.java
+++ app/src/main/java/com/nick/sampleffmpeg/ui/activity/ImportVideoActivty.java
@@ -2,12 +2,17 @@ package com.nick.sampleffmpeg.ui.activity;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.content.ContentUris;
 import android.content.Context;
 import android.content.CursorLoader;
+import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -61,12 +66,13 @@ public class ImportVideoActivty extends BaseActivity {
     private int[] _videosId;
     String selection =  MediaStore.Files.FileColumns.MEDIA_TYPE + "="
             + MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO;
-
+    final int ADD_CLIP_REQUEST=1000;
     protected Context _context;
+    final int SELECT_PICTURE =100;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        _context = getApplicationContext();
+       /* _context = getApplicationContext();
         setContentView(R.layout.activity_import_video);
         _gallery = (GridView) findViewById(R.id.gridView);
         _contentUri = MEDIA_EXTERNAL_CONTENT_URI;
@@ -77,7 +83,18 @@ public class ImportVideoActivty extends BaseActivity {
             public void onClick(View v) {
                 finish();
             }
-        });
+        });*/
+
+       /* Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("video*//*");
+
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Video"), SELECT_PICTURE );*/
+        Intent intent = new Intent();
+        intent.setType("video/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,"Select Video"),SELECT_PICTURE);
+
 
     }
     private void setGalleryAdapter() {
@@ -135,6 +152,231 @@ public class ImportVideoActivty extends BaseActivity {
       }
     };
 
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_PICTURE) {
+                Uri selectedImageUri = data.getData();
+
+
+                filename = getPath(this, selectedImageUri);
+                if (filename != null ) {
+
+
+                    if (VideoUtils.getVideoLength(filename) < 5) {
+                        showAccountAlertDialog("Opps, We can't let you do that", "Be sure your video is least 5 seconds long before importing.");
+                        return;
+                    }
+
+                    try{
+                        String s = filename.substring(filename.lastIndexOf(".") + 1);
+                        finalPath = destinationFilePath(s).getAbsolutePath();
+                        copyDirectory(new File(filename), new File(finalPath));
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+
+                    Constant.setSourceVideo(finalPath);
+
+                    int recordingTime = VideoUtils.getVideoLength(finalPath);
+                    if (recordingTime > 15.f) {
+                        Constant.updateTimeUnit(2);
+                    }
+
+                    if (recordingTime > 25) {
+                        Constant.updateTimeUnit(3);
+                    }
+
+                    if (recordingTime > 40) {
+                        Constant.updateTimeUnit(4);
+                    }
+                    ImportVideoActivty.this.finish();
+                    showActivity(EditingVideoActivity.class, finalPath);
+
+                    //*********** You can do anything when you know the file path :-)
+                    showToast(finalPath);
+
+                }
+            }
+        }
+    }
+
+    private void showAccountAlertDialog(String title, String message) {
+        try {
+
+
+            optionDialog = new Dialog(ImportVideoActivty.this);
+            optionDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            optionDialog.setContentView(R.layout.select_video);
+            ((TextView) optionDialog.findViewById(R.id.textView1)).setText(title);
+            ((TextView) optionDialog.findViewById(R.id.textView2)).setText(message);
+            ((TextView) optionDialog.findViewById(R.id.retry)).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    optionDialog.dismiss();
+                    //finish();
+
+                    Intent intent = new Intent();
+                    intent.setType("video/*");
+                    intent.setAction(Intent.ACTION_GET_CONTENT);
+                    startActivityForResult(Intent.createChooser(intent, "Select Video"), SELECT_PICTURE);
+
+                }
+            });
+            ((TextView) optionDialog.findViewById(R.id.txt_caption)).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    optionDialog.dismiss();
+                    finish();
+
+                }
+            });
+            optionDialog.show();
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+
+    public static String getPath(final Context context, final Uri uri) {
+
+        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+
+        // DocumentProvider
+        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+            // ExternalStorageProvider
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                }
+
+                // TODO handle non-primary volumes
+            }
+            // DownloadsProvider
+            else if (isDownloadsDocument(uri)) {
+
+                final String id = DocumentsContract.getDocumentId(uri);
+                final Uri contentUri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+
+                return getDataColumn(context, contentUri, null, null);
+            }
+            // MediaProvider
+            else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[] {
+                        split[1]
+                };
+
+                return getDataColumn(context, contentUri, selection, selectionArgs);
+            }
+        }
+        // MediaStore (and general)
+        else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            return getDataColumn(context, uri, null, null);
+        }
+        // File
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+
+        return null;
+    }
+
+    /**
+     * Get the value of the data column for this Uri. This is useful for
+     * MediaStore Uris, and other file-based ContentProviders.
+     *
+     * @param context The context.
+     * @param uri The Uri to query.
+     * @param selection (Optional) Filter used in the query.
+     * @param selectionArgs (Optional) Selection arguments used in the query.
+     * @return The value of the _data column, which is typically a file path.
+     */
+    public static String getDataColumn(Context context, Uri uri, String selection,
+                                       String[] selectionArgs) {
+
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = {
+                column
+        };
+
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
+                    null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int column_index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(column_index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
+    }
+
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is ExternalStorageProvider.
+     */
+    public static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is DownloadsProvider.
+     */
+    public static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is MediaProvider.
+     */
+    public static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
+
+    public String getRealPathFromURI(Context context, Uri contentUri) {
+        Cursor cursor = null;
+        try {
+            String[] proj = { MediaStore.Images.Media.DATA };
+            cursor = context.getContentResolver().query(contentUri,  proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+    }
 
     public File destinationFilePath(String extention){
 
